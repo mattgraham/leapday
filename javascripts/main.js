0d6d94668cc87017e098b2ae7ee401fd9c189a73
Adit Cahya Ramadhan <matematika.adit@gmail.com>
2012-04-21 10:08:24 +0700

    fixing navigation issue w/ chromium 18.0.1025.142

--- javascripts/main.js
+++ javascripts/main.js
@@ -21,7 +21,7 @@ $(document).ready(function(){
   
   $("nav ul li a").live("click",function(event) {
     var position = $($(this).attr("href")).offset().top - 190;
-    $("html").animate({scrollTop: position},400);
+    $("body").animate({scrollTop: position},400);
     $("nav ul li a").parent().removeClass("active");
     $(this).parent().addClass("active");
     event.preventDefault();    
